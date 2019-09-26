6ab1f9c4c7aee773ad35db43ef3227bed63b56b0
chcv1234 <chcv1234@gmail.com>
2019-09-26 21:34:36 +0800

    test
    just test

--- script.js
+++ script.js
@@ -1,45 +1,55 @@
-//訪問使用者媒體裝置的相容方法
-function getUserMedia(constrains,success,error){
-    if(navigator.mediaDevices.getUserMedia){
-//最新標準API
-        navigator.mediaDevices.getUserMedia(constrains).then(success).catch(error);
-    } else if (navigator.webkitGetUserMedia){
-//webkit核心瀏覽器
-        navigator.webkitGetUserMedia(constrains).then(success).catch(error);
-    } else if (navigator.mozGetUserMedia){
-//Firefox瀏覽器
-        navagator.mozGetUserMedia(constrains).then(success).catch(error);
-    } else if (navigator.getUserMedia){
-//舊版API
-        navigator.getUserMedia(constrains).then(success).catch(error);
+const video = document.getElementById('video');
+const button = document.getElementById('button');
+const select = document.getElementById('select');
+let currentStream;
+
+function stopMediaTracks(stream) {
+    stream.getTracks().forEach(track => {
+        track.stop();
+    });
+}
+
+function gotDevices(mediaDevices) {
+    select.innerHTML = '';
+    select.appendChild(document.createElement('option'));
+    let count = 1;
+    mediaDevices.forEach(mediaDevice => {
+        if (mediaDevice.kind === 'videoinput') {
+            const option = document.createElement('option');
+            option.value = mediaDevice.deviceId;
+            const label = mediaDevice.label || `Camera ${count++}`;
+            const textNode = document.createTextNode(label);
+            option.appendChild(textNode);
+            select.appendChild(option);
+        }
+    });
+}
+
+button.addEventListener('click', event => {
+    if (typeof currentStream !== 'undefined') {
+        stopMediaTracks(currentStream);
     }
-}
-var video = document.getElementById("video");
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
-//成功的回撥函式
-function success(stream){
-//相容webkit核心瀏覽器
-    var CompatibleURL = window.URL || window.webkitURL;
-//將視訊流設定為video元素的源
-    video.src = CompatibleURL.createObjectURL(stream);
-//播放視訊
-    video.play();
-}
-//異常的回撥函式
-function error(error){
-    console.log("訪問使用者媒體裝置失敗：",error.name,error.message);
-}
-if (navigator.mediaDevices.getUserMedia || navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia){
-//呼叫使用者媒體裝置，訪問攝像頭
-    getUserMedia({
-        video:{width:480,height:320}
-    },success,error);
-} else {
-    alert("你的瀏覽器不支援訪問使用者媒體裝置");
-}
-//註冊拍照按鈕的單擊事件
-document.getElementById("capture").addEventListener("click",function(){
-//繪製畫面
-    context.drawImage(video,0,0,480,320);
+    const videoConstraints = {};
+    if (select.value === '') {
+        videoConstraints.facingMode = 'environment';
+    } else {
+        videoConstraints.deviceId = { exact: select.value };
+    }
+    const constraints = {
+        video: videoConstraints,
+        audio: false
+    };
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(stream => {
+            currentStream = stream;
+            video.srcObject = stream;
+            return navigator.mediaDevices.enumerateDevices();
+        })
+        .then(gotDevices)
+        .catch(error => {
+            console.error(error);
+        });
 });
+
+navigator.mediaDevices.enumerateDevices().then(gotDevices);
